97b0bc68814cc0f3ecc9a215f5a2880650fa4a57
Mila-Zagrevskaya <mila.zagrevskaya@gmail.com>
2019-03-04 22:41:34 +0200

    homework 3

--- js/index.js
+++ js/index.js
@@ -6,14 +6,14 @@ let collection = Array.from (document.getElementsByClassName ('scheme'))
 collection.forEach (
   function (elem) {
     elem.onclick = function (event) {
-      this.style.width = "1000px";
+      this.style.width = "900px";
     }
   }
 )
 collection.forEach (
   function (elem) {
     elem.onmouseout = function (event) {
-      this.style.width = "900px";
+      this.style.width = "800px";
     }
   }
 )
